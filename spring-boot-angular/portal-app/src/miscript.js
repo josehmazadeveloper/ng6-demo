dbdfade9d018f2ffac3dfa80ad66fa3ae1784d04
KRUGER\jmaza <jmaza@kruger.com.ec>
2018-06-01 17:39:22 -0500

    errroes en mi script

---
+++ spring-boot-angular/portal-app/src/miscript.js
@@ -0,0 +1,44 @@
+console.log("hello world")
+
+var jose = "846"
+var nuevo =123;
+
+let ko = jose;
+
+var nuevo = 51516;
+function foo() {
+  for (var i = 0; i < 5; i++) {
+    yield i * 2;
+  }
+}
+
+nuevot.forEach(element => {
+  console.log("aiubhsias"+ element)
+});
+
+var merged = arr.reduce(function(a, b) {
+  a.concat(b);
+});
+
+var merged2 = arr.reduce(function(a, b) {
+  a.concat(b);
+  a.concat(b);
+  a.concat(b);
+  a.concat(b);
+});
+
+var x = "Foo";
+"length" in x;
+0 in x;
+
+if (x < 0) {
+  new Error("x must be nonnegative");
+}
+
+//esto es en local
+
+if(unexpectedCondition) {
+  alert("Unexpected Condition");
+}
+var obj =  new Function("return " + data)();
+console.log("ola k ase");
